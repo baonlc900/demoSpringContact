d255f5bee042254547bed2860fe4834e9cc18003
baonlc900 <congbaoit90@gmail.com>
2018-11-22 16:31:29 +0700

    edit Contact

--- src/main/java/com.example.demo.domain/Contact.java
+++ src/main/java/com.example.demo.domain/Contact.java
@@ -1,10 +1,15 @@
 package com.example.demo.domain;
 
+import org.springframework.validation.annotation.Validated;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
+@Validated
 public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,10 +21,14 @@ public class Contact {
     @Size(min = 2, message = "First Name must be at least 2 characters.")
     private String firstName;
     @Column(name = "mobile_no")
-    @Size(min = 11, max = 11, message = "Mobile no. must be 11 digits.")
+    @Size(min = 10, max = 11, message = "Mobile no. must be 10-11 digits.")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile no. must be number")
     private String mobileNumber;
     @NotEmpty(message = "Address is required.")
     private String address;
+    @NotEmpty(message = "Email is required.")
+    @Email(message = "Email not valid.")
+    private String email;
 
     public Integer getId() {
         return id;
@@ -60,4 +69,8 @@ public class Contact {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 }
